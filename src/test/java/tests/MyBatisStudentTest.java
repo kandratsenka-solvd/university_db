16f374927d8c3e71a80ff8e1c3bed3fe84d8a183
kandratsenka-solvd <akandratsenka.laba@solvd.com>
2023-06-24 23:39:35 +0300

    add: SqlSessionPool implementation

--- src/test/java/tests/MyBatisStudentTest.java
+++ src/test/java/tests/MyBatisStudentTest.java
@@ -1,36 +1,37 @@
 package tests;
 
+import connection.SqlSessionPool;
 import mappers.IStudentMapper;
-import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
-import org.apache.ibatis.session.SqlSessionFactory;
-import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.testng.annotations.Test;
 import utils.PersonUtil;
 
-import java.io.IOException;
-import java.io.Reader;
 
 public class MyBatisStudentTest {
 
     private static final Logger LOGGER = LogManager.getLogger();
 
     @Test
+    public void testAddStudent1() {
+        SqlSession sqlSession = SqlSessionPool.getInstance().getSqlSession();
+        IStudentMapper iStudentMapper = sqlSession.getMapper(IStudentMapper.class);
+        iStudentMapper.add(PersonUtil.generateStudent());
+        int i = iStudentMapper.getGeneratedKey();
+        LOGGER.info("student_id: " + i);
+        sqlSession.commit();
+        sqlSession.close();
+    }
+
+    @Test
     public void testAddStudent() {
-        try (Reader reader = Resources.getResourceAsReader("mybatis-config.xml")) {
-            SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);
-            SqlSession sqlSession = sqlSessionFactory.openSession(false);
-            System.out.println(sqlSession.hashCode());
-            IStudentMapper iStudentMapper = sqlSession.getMapper(IStudentMapper.class);
-            iStudentMapper.add(PersonUtil.generateStudent());
-            int i = iStudentMapper.getGeneratedKey();
-            LOGGER.info("student_id: " + i);
-            sqlSession.commit();
-            sqlSession.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        SqlSession sqlSession = SqlSessionPool.getInstance().getSqlSession();
+        IStudentMapper iStudentMapper = sqlSession.getMapper(IStudentMapper.class);
+        iStudentMapper.add(PersonUtil.generateStudent());
+        int i = iStudentMapper.getGeneratedKey();
+        LOGGER.info("student_id: " + i);
+        sqlSession.commit();
+        sqlSession.close();
     }
 }
